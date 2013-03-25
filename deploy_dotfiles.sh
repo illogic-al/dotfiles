9dabef66a679a6e7d75225f9f9fc487f0b01d69f
Orville Bennett <illogical1@ymail.com>
2013-03-25 01:47:25 -0400

    Exclude gvimrc from being copied. Fix logic error for final msg.

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -10,21 +10,25 @@ do
   then
     if [[ -d $dotfiles ]]
     then
-      echo "$dotfiles exists ..."
-      if [[ -d $oldfiles ]]
+      if [[ (($i != 'gvimrc')) ]]
       then
-        echo "Moving old dotfiles to $oldfiles ..."
-        mv ~/.$i $oldfiles
-      else
-        echo "Moving old dotfiles to $oldfiles ..."
-        mkdir $oldfiles
-        mv ~/.$i $oldfiles
-      fi
+        echo "$dotfiles exists ..."
+        if [[ -d $oldfiles ]]
+        then
+          echo "Moving old dotfiles to $oldfiles ..."
+          mv ~/.$i $oldfiles
+        else
+          echo "Moving old dotfiles to $oldfiles ..."
+          mkdir $oldfiles
+          mv ~/.$i $oldfiles
+        fi
 
-      echo "Linking dotfiles in $dotfiles"
-      ln -s $dotfiles/$i ~/.$i
+        echo "Linking dotfiles in $dotfiles ..."
+        ln -s $dotfiles/$i ~/.$i
+        echo "All done!"
+      fi
+    else
+      echo "Whoops! Your dotfiles directory is not at $dotfiles."
     fi
-  else
-    echo "Whoops! Your dotfiles directory is not at $dotfiles."
   fi
 done
